065b6f9b3a5a395f67c998510199bbed0dba66dd
robertarus <128049744+robertarus@users.noreply.github.com>
2023-05-17 13:46:34 +0300

    Delete 05. Controllers & IActionResult directory

--- 05. Controllers & IActionResult/02. ContentResult/ControllersExample/Program.cs
+++
@@ -1,7 +0,0 @@
-var builder = WebApplication.CreateBuilder(args);
-builder.Services.AddControllers(); //adds all the controller classes as services
-
-var app = builder.Build();
-app.UseRouting();
-app.MapControllers();
-app.Run();
--- 05. Controllers & IActionResult/04. File Results/ControllersExample/Program.cs
+++
@@ -1,8 +0,0 @@
-var builder = WebApplication.CreateBuilder(args);
-builder.Services.AddControllers(); //adds all the controller classes as services
-
-var app = builder.Build();
-app.UseStaticFiles();
-app.UseRouting();
-app.MapControllers();
-app.Run();
