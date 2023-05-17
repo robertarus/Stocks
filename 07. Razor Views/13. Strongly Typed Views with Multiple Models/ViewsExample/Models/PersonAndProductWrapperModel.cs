89c1f071cd13b5122a15a36467ef1889775e4a20
robertarus <128049744+robertarus@users.noreply.github.com>
2023-05-17 13:47:12 +0300

    Delete 07. Razor Views directory

--- 07. Razor Views/13. Strongly Typed Views with Multiple Models/ViewsExample/Models/PersonAndProductWrapperModel.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace ViewsExample.Models
-{
-  public class PersonAndProductWrapperModel
-  {
-    public Person PersonData { get; set; }
-    public Product ProductData { get; set; }
-  }
-}
--- 07. Razor Views/13. Strongly Typed Views with Multiple Models/ViewsExample/Models/Product.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace ViewsExample.Models
-{
-  public class Product
-  {
-    public int ProductId { get; set; }
-    public string? ProductName { get; set; }
-  }
-}
