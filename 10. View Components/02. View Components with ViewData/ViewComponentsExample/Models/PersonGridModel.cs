cd373cb4e62b363046ed8e08d8895b68a134adb7
robertarus <128049744+robertarus@users.noreply.github.com>
2023-05-17 13:48:04 +0300

    Delete 10. View Components directory

--- 10. View Components/02. View Components with ViewData/ViewComponentsExample/Models/PersonGridModel.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace ViewComponentsExample.Models
-{
-  public class PersonGridModel
-  {
-    public string GridTitle { get; set; } = string.Empty;
-    public List<Person> Persons { get; set; } = new List<Person>();
-  }
-}
--- 10. View Components/03. Strongly Typed View Components/ViewComponentsExample/Models/Person.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace ViewComponentsExample.Models
-{
-  public class Person
-  {
-    public string? PersonName { get; set; }
-    public string? JobTitle { get; set; }
-  }
-}
