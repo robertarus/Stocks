c7aa5d641de080522adeea1d24afee34d085bb5c
robertarus <128049744+robertarus@users.noreply.github.com>
2023-05-17 13:46:53 +0300

    Delete 06. Model Binding and Validations directory

--- 06. Model Binding and Validations/12. FromBody/ModelValidationsExample/Program.cs
+++
@@ -1,9 +0,0 @@
-var builder = WebApplication.CreateBuilder(args);
-builder.Services.AddControllers();
-var app = builder.Build();
-
-app.UseStaticFiles();
-app.UseRouting();
-app.MapControllers();
-
-app.Run();
